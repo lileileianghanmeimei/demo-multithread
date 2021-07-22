a800768e6f39c6c845fb620f04ef451e1f28872a
mei <mei.li@convertlab.com>
2021-07-22 18:04:11 +0800

    feature:create new project to practice

---
+++ src/main/java/com/example/demomultithread/DemoMultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomultithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMultithreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMultithreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomultithread/DemoMultithreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomultithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMultithreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
