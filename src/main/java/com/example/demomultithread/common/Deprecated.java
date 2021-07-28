9827af4daf4250f9285bd24e835bb8f1c5f88192
mei <mei.li@convertlab.com>
2021-07-28 16:59:21 +0800

    fix:     提交

--- src/main/java/com/example/demomultithread/common/Deprecated.java
+++ src/main/java/com/example/demomultithread/common/Deprecated.java
@@ -1,2 +1,43 @@
-package com.example.demomultithread.common;public class Deprecated {
+package com.example.demomultithread.common;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 过期的suspend()、resume()和stop()
+ *
+ * 创建了一个线程PrintThread，它以1秒的频率进行打印，而
+ * 主线程对其进行暂停、恢复和停止操作。
+ */
+public class Deprecated {
+    public static void main(String[] args) throws Exception {
+        DateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Thread printThread = new Thread(new Runner(), "PrintThread"); printThread.setDaemon(true);
+        printThread.start();
+        TimeUnit.SECONDS.sleep(3);
+        // 将PrintThread进行暂停，输出内容工作停止
+        printThread.suspend();
+        System.out.println("main suspend PrintThread at " + format.format(new Date())); TimeUnit.SECONDS.sleep(3);
+        // 将PrintThread进行恢复，输出内容继续
+        printThread.resume();
+        System.out.println("main resume PrintThread at " + format.format(new Date())); TimeUnit.SECONDS.sleep(3);
+        // 将PrintThread进行终止，输出内容停止
+        printThread.stop();
+        System.out.println("main stop PrintThread at " + format.format(new Date())); TimeUnit.SECONDS.sleep(3);
+    }
+
+    static class Runner implements Runnable {
+        @Override
+        public void run() {
+            DateFormat format = new SimpleDateFormat("HH:mm:ss");
+            while (true) {
+            System.out.println(Thread.currentThread().getName() + " Run at " +
+            format.format(new Date()));
+            SleepUtils.second(1);
+            }
+        }
+    }
+
 }
